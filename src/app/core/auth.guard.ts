d6dd1c0ab30127f0f60ae6f2b76583b420a0825a
abikananda <abikananda@users.noreply.github.com>
2026-09-09 04:42:28 +0530

    feat: add src/app/core/auth.guard.ts

---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,1 @@
+import { CanActivateFn,Router } from '@angular/router'; import { inject } from '@angular/core'; import { AuthService } from './auth.service'; export const authGuard:CanActivateFn=()=>inject(AuthService).loggedIn()||inject(Router).createUrlTree(['/login']);

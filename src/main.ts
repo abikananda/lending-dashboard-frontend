c23318b6cf0cea6782705173adfe568e1f69f472
abikananda <abikananda@users.noreply.github.com>
2026-09-12 03:36:22 +0530

    Use Indian number formatting

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser'; import { provideRouter } from '@angular/router'; import { provideHttpClient,withInterceptors } from '@angular/common/http'; import { AppComponent } from './app/app.component'; import { routes } from './app/app.routes'; import { authInterceptor } from './app/core/auth.interceptor'; bootstrapApplication(AppComponent,{providers:[provideRouter(routes),provideHttpClient(withInterceptors([authInterceptor]))]}).catch(console.error);
+import { registerLocaleData } from '@angular/common';
+import localeEnIn from '@angular/common/locales/en-IN';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { authInterceptor } from './app/core/auth.interceptor';
+import { routes } from './app/app.routes';
+
+registerLocaleData(localeEnIn);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-IN' },
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
+}).catch(console.error);
